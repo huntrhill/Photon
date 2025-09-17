f0def5e8893c4d8ac2770232e60bdb80001bd385
Cody Uhl <cuhl@uark.edu>
2025-09-17 09:10:03 -0500

    Update config.py

--- PhotonGame/config.py
+++ PhotonGame/config.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+SEND_ADDR  = os.getenv("PHOTON_SEND_ADDR", "127.0.0.1")
+SEND_PORT  = int(os.getenv("PHOTON_SEND_PORT", "7500"))
+RECV_ADDR  = os.getenv("PHOTON_BIND_ADDR", "0.0.0.0")
+RECV_PORT  = int(os.getenv("PHOTON_RECV_PORT", "7501"))
+
+PG = dict(
+    host=os.getenv("PGHOST", "127.0.0.1"),
+    port=int(os.getenv("PGPORT", "5432")),
+    dbname=os.getenv("PGDATABASE", "photon"),
+    user=os.getenv("PGUSER", "postgres"),
+    password=os.getenv("PGPASSWORD", "postgres"),
+)
