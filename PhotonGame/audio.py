f3c059d4aeb245515ee519b69b4807064ca612a5
Cody Uhl <cuhl@uark.edu>
2025-09-18 12:46:01 -0500

    Update audio.py

--- PhotonGame/audio.py
+++ PhotonGame/audio.py
@@ -1,1 +1,10 @@
+import os, random, pygame
+def init_tracks(path):
+    pygame.mixer.init()
+    p = os.path.join(path, "tracks")
+    return [os.path.join(p,f) for f in os.listdir(p) if f.lower().endswith(".mp3")]
 
+def play_random(tracks):
+    if not tracks: return
+    pygame.mixer.music.load(random.choice(tracks))
+    pygame.mixer.music.play()
